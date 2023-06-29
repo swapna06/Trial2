7c35890b4e3620642841940c1a7f75760fe2b442
swapna06 <swapna06@users.noreply.github.com>
2023-06-28 18:35:23 -0700

    Synchronizing job test-schalla-notebook-6-28 from AWS Glue at 2023/06/29 01:35:23

---
+++ test-schalla-notebook-6-28/test-schalla-notebook-6-28.py
@@ -0,0 +1,31 @@
+
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+  
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+dyf = glueContext.create_dynamic_frame.from_catalog(database='database_name', table_name='table_name')
+dyf.printSchema()
+df = dyf.toDF()
+df.show()
+s3output = glueContext.getSink(
+  path="s3://bucket_name/folder_name",
+  connection_type="s3",
+  updateBehavior="UPDATE_IN_DATABASE",
+  partitionKeys=[],
+  compression="snappy",
+  enableUpdateCatalog=True,
+  transformation_ctx="s3output",
+)
+s3output.setCatalogInfo(
+  catalogDatabase="demo", catalogTableName="populations"
+)
+s3output.setFormat("glueparquet")
+s3output.writeFrame(DyF)
+job.commit()
